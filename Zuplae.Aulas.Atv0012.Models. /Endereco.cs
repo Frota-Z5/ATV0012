7e725195d364913261eff45fefd648048fac2e96
Leandro Barbosa de Lurde <le.barbosa@icloud.com>
2025-06-30 21:42:41 -0300

    criado estrutura do projeto

---
+++ Zuplae.Aulas.Atv0012.Models. /Endereco.cs
@@ -0,0 +1,28 @@
+﻿namespace Zuplae.Aulas.Atv0012.Models_;
+
+public class Endereco
+{
+    public string rua;
+    public string numero;
+    public string bairro;
+    public string cidade;
+    public string estado;
+    public string cep;
+    
+    public Endereco ( ){ }
+
+    public Endereco(string rua,  string numero, string bairro,string cidade,string estado,string cep)
+    {
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;    
+    }   
+    
+    public override string ToString()
+    {
+        return $"Rua: {rua}, {numero} - {bairro}, {cidade} - {estado}, CEP: {cep}";
+    }
+}
